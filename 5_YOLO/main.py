210bf603c04fdb88c5313c16efb954a6ff9c4bfc
AbedSoleymani <zsoleymani@ualberta.ca>
2023-08-03 13:53:23 -0600

    finalizing 5_YOLO

--- 5_YOLO/main.py
+++ 5_YOLO/main.py
@@ -2,8 +2,8 @@ from model_setup import model_setup
 from img_obj_detection import img_obj_detection
 
 model, class_names = model_setup()
-# img_obj_detection(model=model,
-#                   class_names=class_names,
-#                   dir='./5_YOLO/imgs/wine.jpg')
+img_obj_detection(model=model,
+                  class_names=class_names,
+                  dir='./5_YOLO/imgs/wine.jpg')
 
 print(class_names)
