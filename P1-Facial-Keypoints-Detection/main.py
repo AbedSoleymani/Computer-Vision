6bdb191a40bbe9c687b63b5f98b3784a73605f7a
AbedSoleymani <zsoleymani@ualberta.ca>
2023-08-19 22:23:47 -0600

    creating data transformation object and applying that to FacialKeypointDataset class to create a ready-to-train dataset

--- P1-Facial-Keypoints-Detection/main.py
+++ P1-Facial-Keypoints-Detection/main.py
@@ -5,10 +5,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import cv2
+from torchvision import transforms
 
 from generate_dataset import generate_dataset
 from utils import show_rand_img, show_keypoints, show_preprocessing_results
 from facial_keypoints_dataset import FacialKeypointsDataset
+from preprocessing import Rescale, RandomCrop, FaceCrop, Normalize, ToTensor, Random90DegFlip
 
 
 
@@ -17,12 +19,24 @@ generate_dataset()
 
 os.chdir("./P1-Facial-Keypoints-Detection")
 face_dataset = FacialKeypointsDataset(csv_file='./data/training_frames_keypoints.csv',
-                                      root_dir='data/training/')
+                                      root_dir='./data/training/',
+                                      transform=None)
 
 # show_rand_img(dataset=face_dataset)
 
-show_preprocessing_results(dataset=face_dataset,
-                           test_num=0)
+# show_preprocessing_results(dataset=face_dataset, test_num=0)
 
+"""Now, we are ready to create a processed dataset suitable for training"""
+data_transform = transforms.Compose([Rescale(250),
+                                     RandomCrop(224),
+                                     Normalize(),
+                                     ToTensor()])
+transformed_dataset = FacialKeypointsDataset(csv_file='./data/training_frames_keypoints.csv',
+                                             root_dir='./data/training/',
+                                             transform=data_transform)
+
+"""Note that in the transformed dataset, facial keypoints
+belong into [-2, 2] and they do not match the image"""
+show_rand_img(dataset=transformed_dataset)
 
 
