5ad5f36f635ac6df2ab9ac90b3c57690b339f423
AbedSoleymani <zsoleymani@ualberta.ca>
2023-08-19 21:45:11 -0600

    moving plt.show to show all of the 3 subplots in preprocessing plot in one window

--- P1-Facial-Keypoints-Detection/utils.py
+++ P1-Facial-Keypoints-Detection/utils.py
@@ -7,9 +7,9 @@ def show_rand_img(dataset):
     rand_i = np.random.randint(0, len(dataset))
     sample = dataset[rand_i]
     show_keypoints(sample['image'], sample['keypoints'])
+    plt.show()
 
 def show_keypoints(image, key_pts):
     """Shows a given image with its corresponding keypoints"""
     plt.imshow(image)
     plt.scatter(key_pts[:, 0], key_pts[:, 1], s=20, marker='.', c='m')
-    plt.show()
