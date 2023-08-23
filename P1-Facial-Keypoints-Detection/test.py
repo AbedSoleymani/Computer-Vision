9fb8d36562f4e955f2835cacfb5f54eccaf27815
AbedSoleymani <zsoleymani@ualberta.ca>
2023-08-23 13:42:26 -0600

    modifying test.py to test the model trained via transfer learning approach

--- P1-Facial-Keypoints-Detection/test.py
+++ P1-Facial-Keypoints-Detection/test.py
@@ -1,4 +1,4 @@
-from models import NaimishNet
+from models import NaimishNet, resnet18_grayscale
 import torch
 from gen_tr_val_ts_datasets import create_datasets
 from net_utils import *
@@ -9,8 +9,10 @@ img_size = 224
 
 train_loader, valid_loader, test_loader = create_datasets(batch_size=batch_size,
                                                           img_size=img_size)
-net = NaimishNet(img_size, use_maxp=False)
 
+# You have to change the model_name parameter as well
+net = NaimishNet(img_size, use_maxp=False)
+net = resnet18_grayscale()
 
 test_images, test_outputs, gt_pts = net_sample_output(net=net,
                                                       data_loader=train_loader)
@@ -22,7 +24,8 @@ visualize_output(images=test_images,
 
 n_epochs = 2
 model_dir = './P1-Facial-Keypoints-Detection/saved_models/'
-model_name = 'abed_model_epochs' + str(n_epochs)
+# model_name = 'abed_model_epochs2'
+model_name = 'abed_resnet18_epochs2'
 
 net.load_state_dict(torch.load(model_dir + model_name))
 test_images, test_outputs, gt_pts = net_sample_output(net=net,
