6676b07bf28caf7eda81d41d1cbb0482f343b5f9
AbedSoleymani <zsoleymani@ualberta.ca>
2023-12-24 00:13:33 -0700

    a module for preprocessing images and masks using albumentations library

---
+++ 12_UNet/2D/CustomData/transforms.py
@@ -0,0 +1,33 @@
+import albumentations as A # pip install albumentations
+from albumentations.pytorch import ToTensorV2
+
+def transforms(image_height, image_width):
+
+    train_transform = A.Compose(
+            [
+                A.Resize(height=image_height, width=image_width),
+                A.Rotate(limit=35, p=1.0),
+                A.HorizontalFlip(p=0.5),
+                A.VerticalFlip(p=0.1),
+                A.Normalize(
+                    mean=[0.0, 0.0, 0.0],
+                    std=[1.0, 1.0, 1.0],
+                    max_pixel_value=255.0,
+                ),
+                ToTensorV2(),
+            ],
+        )
+
+    val_transforms = A.Compose(
+        [
+            A.Resize(height=image_height, width=image_width),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+
+    return train_transform, val_transforms
